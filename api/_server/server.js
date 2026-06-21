147b3453b2bd204ae86aca5963635a09272ba7ab
ooffnight <ooffnightt@gmail.com>
2026-06-21 23:20:45 +0300

    Fix path depth

--- api/_server/server.js
+++ api/_server/server.js
@@ -52,8 +52,8 @@ app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-// ── ВАЖНО: Vercel меняет вложенность папок, поэтому путь вычисляем динамически ──
-const projectRoot = process.env.VERCEL ? path.join(__dirname, '..') : path.join(__dirname, '../..');
+// ── ВАЖНО: Вычисляем корень проекта ──
+const projectRoot = path.join(__dirname, '../..');
 
 // ── Статические файлы ──────────────────────────────
 app.use(express.static(projectRoot));
